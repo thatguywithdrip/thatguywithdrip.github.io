f8e36b5f1141e54b8972d9dd624319a2de23f2af
thatguywithdrip <96744336+thatguywithdrip@users.noreply.github.com>
2022-03-20 19:19:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const {readFile} = require('fs');
+const express = require('express');
+const app = express();
+const Port = 3600;
+
+app.get('/', (request, response) => {
+
+    readFile('./index.html', 'utf8', (err, html) => {
+
+        if (err){
+
+            response.status(500).send('error, lol');
+        }
+
+        response.send(html);
+    })
+});
+app.get('/Zyciorys', (request, response) => {
+
+    readFile('./Zyciorys.html', 'utf8', (err, html) => {
+
+        if (err){
+
+            response.status(500).send('error, lol');
+        }
+
+        response.send(html);
+    })
+});
+
+
+app.listen(process.env.PORT || Port, () => console.log("at http://localhost:" + Port)) 
